c6f2a988bd7d8690c6abf6c4ac7dd3bac37e60eb
Sneha <snehalall.sneha@gmail.com>
2023-02-10 19:00:17 +0530

    fix: remove empty lines

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,58 +1,51 @@
-const getFunction=require('./../services/userService');
+const getFunction = require("./../services/userService");
 
-const postCredentials=async(req,res)=>
-{
-    try{
-        const{username,password}=req.body;
-        const result=await getFunction.postCredentials(username,password);
-        res.status(201).json({
-            message:"inserted",
-            data :{
-                credential: result
-            }
-    })
-    }catch(error)
-    {
-        res.status(500).json({
-            error:error.message
-        })
-    }
-}
-const userLogin=async(req,res)=>
-{
-    try{
-        const{username,password}=req.body;
-        const result=await getFunction.loginCredentials(username,password);
-        res.status(201).json({
-            message:"logged in",
-            data :{
-                JWT_token : result
-            }
-    })
-    }catch(error)
-    {
-        res.status(500).json({
-            error:error.message
-        })
-    }
-}
+const postCredentials = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const result = await getFunction.postCredentials(username, password);
+    res.status(201).json({
+      message: "inserted",
+      data: {
+        credential: result,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+const userLogin = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const result = await getFunction.loginCredentials(username, password);
+    res.status(201).json({
+      message: "logged in",
+      data: {
+        JWT_token: result,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
 
-const validateToken=async(req,res)=>{
-    try{
-        const {token}=req.query;
-        const valid=await getFunction.validateToken(token);
-        res.status(200).json({
-            status:'success',
-            message:'user verified',
-            data:valid
-        })
-
-    }catch(error)
-    {
-        res.status(500).json({
-            error:error.message
-        })
-    }
-}
-
-module.exports={postCredentials,userLogin,validateToken};
+const validateToken = async (req, res) => {
+  try {
+    const { token } = req.query;
+    const valid = await getFunction.validateToken(token);
+    res.status(200).json({
+      status: "success",
+      message: "user verified",
+      data: valid,
+    });
+  } catch (error) {
+    res.status(500).json({
+      error: error.message,
+    });
+  }
+};
+module.exports = { postCredentials, userLogin, validateToken };
